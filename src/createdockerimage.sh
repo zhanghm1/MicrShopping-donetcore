a31fd3fb705ed71b62c56cbbaec5ca0fedff1ce3
zhm <@qq.com>
2020-03-26 14:56:57 +0800

    test

--- src/createdockerimage.sh
+++ src/createdockerimage.sh
@@ -1,3 +1,4 @@
+
 docker build -f MicrShopping.OrderApi/Dockerfile -t micrshopping.orderapi . --no-cache
 docker build -f MicrShopping.Identity/Dockerfile -t micrshopping.identity . --no-cache
 docker build -f MicrShopping.PayApi/Dockerfile -t micrshopping.payapi . --no-cache
@@ -10,3 +11,43 @@ npm i
 npm run build
 cd dist
 docker build -f Dockerfile -t micrshopping.webvue . --no-cache
+
+
+#登录镜像仓库
+docker  login -u admin -p 123456 http://127.0.0.1:30003
+
+# 获取版本号，将tag用版本号代替
+old_docker_tag=v0.1.1
+new_docker_tag=v0.1.2
+
+#添加tag
+docker tag micrshopping.orderapi 127.0.0.1:30003/micrshopping/micrshopping.orderapi:v0.1.2
+docker tag micrshopping.identity 127.0.0.1:30003/micrshopping/micrshopping.identity:v0.1.2
+docker tag micrshopping.payapi 127.0.0.1:30003/micrshopping/micrshopping.payapi:v0.1.2
+docker tag micrshopping.productapi 127.0.0.1:30003/micrshopping/micrshopping.productapi:v0.1.2
+docker tag micrshopping.usermanageapi 127.0.0.1:30003/micrshopping/micrshopping.usermanageapi:v0.1.2
+docker tag micrshopping.webmvc 127.0.0.1:30003/micrshopping/micrshopping.webmvc:v0.1.2
+docker tag micrshopping.webvue 127.0.0.1:30003/micrshopping/micrshopping.webvue:v0.1.2
+
+#推送
+docker push 127.0.0.1:30003/micrshopping/micrshopping.orderapi:v0.1.2
+docker push 127.0.0.1:30003/micrshopping/micrshopping.identity:v0.1.2
+docker push 127.0.0.1:30003/micrshopping/micrshopping.payapi:v0.1.2
+docker push 127.0.0.1:30003/micrshopping/micrshopping.productapi:v0.1.2
+docker push 127.0.0.1:30003/micrshopping/micrshopping.usermanageapi:v0.1.2
+docker push 127.0.0.1:30003/micrshopping/micrshopping.webmvc:v0.1.2
+docker push 127.0.0.1:30003/micrshopping/micrshopping.webvue:v0.1.2
+
+# docker pull 127.0.0.1:30003/library/micrshopping.orderapi:v0.1.2
+
+
+cd ../../../../charts/micr-shopping
+
+cp -r v0.1.1/ v0.1.2
+
+sed -i 's/0.1.1/0.1.2/g' v0.1.2/Chart.yaml
+
+#执行字符串
+cmd="echo hello word"
+
+$cmd
